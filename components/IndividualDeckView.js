b18151e40723396ac7d3e6f4347b98cd78605eab
Yoonwhan-Ahn <ahn393@gmail.com>
2018-01-01 09:14:08 -0800

    Switch IndividualDeckView to a class

--- components/IndividualDeckView.js
+++ components/IndividualDeckView.js
@@ -6,41 +6,44 @@ import { addCardToDeck } from '../utils/api'
 import { Ionicons } from '@expo/vector-icons'
 import { NavigationActions } from 'react-navigation'
 
-function IndividualDeckView(props) {
-  const backAction = NavigationActions.back()
-  const { navigation, decks } = props
-  const { deckTitle } = navigation.state.params
-  const numCards = decks[deckTitle].questions.length
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => navigation.navigate('DeckList')}>
-        <Ionicons name="ios-arrow-back" size={35} style={{ padding: 10, paddingTop: 30 }} />
-      </TouchableOpacity>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <View>
-        <Text style={{ fontSize: 28, fontWeight: '900', color: '#333' }}>
-          {deckTitle}
-        </Text>
-        <Text style={{ fontSize: 24, fontWeight: '900', color: '#555', alignSelf: 'center', marginTop: 10 }}>
-          {numCards} cards
-        </Text>
+class IndividualDeckView extends React.Component {
+
+  render() {
+    const backAction = NavigationActions.back()
+    const { navigation, decks } = this.props
+    const { deckTitle } = navigation.state.params
+    const numCards = decks[deckTitle].questions.length
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => navigation.navigate('DeckList')}>
+          <Ionicons name="ios-arrow-back" size={35} style={{ padding: 10, paddingTop: 30 }} />
+        </TouchableOpacity>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <View>
+          <Text style={{ fontSize: 28, fontWeight: '900', color: '#333' }}>
+            {deckTitle}
+          </Text>
+          <Text style={{ fontSize: 24, fontWeight: '900', color: '#555', alignSelf: 'center', marginTop: 10 }}>
+            {numCards} cards
+          </Text>
+          </View>
+        </View>
+        {/* button to create a new deck with */}
+        <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+          <TouchableOpacity onPress={() => navigation.navigate('NewQuestion', {deckTitle})}>
+              <View style={styles.button}>
+                <Text style={{color: '#fff', fontSize: 18, fontWeight: '800'}}>Create New Question</Text>
+              </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('Quiz', {deckTitle, numCards, numCorrect: 0, currentPage: 0 })}>
+              <View style={styles.button}>
+                <Text style={{color: '#fff', fontSize: 18, fontWeight: '800'}}>Start Quiz</Text>
+              </View>
+          </TouchableOpacity>
         </View>
       </View>
-      {/* button to create a new deck with */}
-      <View style={{ flex: 1, justifyContent: 'flex-end' }}>
-        <TouchableOpacity onPress={() => navigation.navigate('NewQuestion', {deckTitle})}>
-            <View style={styles.button}>
-              <Text style={{color: '#fff', fontSize: 18, fontWeight: '800'}}>Create New Question</Text>
-            </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Quiz', {deckTitle, numCards, numCorrect: 0, currentPage: 0 })}>
-            <View style={styles.button}>
-              <Text style={{color: '#fff', fontSize: 18, fontWeight: '800'}}>Start Quiz</Text>
-            </View>
-        </TouchableOpacity>
-      </View>
-    </View>
-  )
+    )
+  }
 }
 
 const mapStateToProps = state => ({
